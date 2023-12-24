ccfd1495082d5a0cb092b2155d4df65420b933d9
chagantipati.rajesh09@gmail.com <chagantipati.rajesh09@gmail.com>
2023-12-24 17:01:07 +0530

    home page updated

--- src/components/Atoms/Header.jsx
+++ src/components/Atoms/Header.jsx
@@ -1,22 +1,67 @@
-import { useNavigate } from 'react-router-dom';
-import "./Header.css"
+import { useNavigate } from "react-router-dom";
+import battalaDukanam from "../../assets/battalaDukanam.gif";
+import "./Header.css";
+import { useState } from "react";
 
 const Header = () => {
-    const navigate = useNavigate();
-    const validateSignIn = () => {
-        navigate("/E-commerce-BD-/SignIn");
-    }
+  const navPageClass = "navPage";
+  const navigate = useNavigate();
+  const [navPage, setNavPage] = useState("");
 
-    return (
-        < nav className="navbar navbar-dark bg-dark fixed-top">
-            <a className="navbar-brand" href="#">BD</a>
-            <button
-                type="button"
-                className="btn btn-secondary"
-                onClick={() => validateSignIn()}>
-                Sign In
-            </button>
-        </nav >
-    );
-}
+  const [menu, setMenu] = useState("container");
+  const validateSignIn = () => {
+    navBarClicked("");
+    navigate("/E-commerce-BD-/SignIn");
+  };
+
+  const navBarClicked = (navPage) => {
+    setNavPage(navPage);
+  };
+
+  const menuClick = () => {
+    setMenu(menu === "change" ? "container" : "change");
+  };
+  return (
+    <nav className="navbar fixed-top">
+      <a className="navbar-brand" href="#" onClick={() => navBarClicked("")}>
+        <img className="bdLogo" src={battalaDukanam} />
+      </a>
+      <div className="d-grid d-md-flex navigationPages">
+        <a
+          className={`${navPageClass}${navPage === "ev" ? "clicked" : ""}`}
+          href="#"
+          onClick={() => navBarClicked("ev")}
+        >
+          Every thing
+        </a>
+        <a
+          className={`${navPageClass}${navPage === "men" ? "clicked" : ""}`}
+          href="#"
+          onClick={() => navBarClicked("men")}
+        >
+          Men
+        </a>
+        <a
+          className={`${navPageClass}${navPage === "women" ? "clicked" : ""}`}
+          href="#"
+          onClick={() => navBarClicked("women")}
+        >
+          Women
+        </a>
+        <a
+          type="button"
+          className="btn btn-secondary"
+          onClick={() => validateSignIn()}
+        >
+          Sign In
+        </a>
+        <div className={menu} onClick={() => menuClick()}>
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 export default Header;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
 import "./Home.css";
-import homeImg from '../../assets/home-img-used.png'
+// import homeImg from '../../assets/homeImg.png'
 import Header from "../../components/Atoms/Header";
+import HomeCard from "../../components/Molecules/Cards/HomeCard";
+import BrandCarousel from "../../components/Molecules/BrandCarousel/BrandCarousel";
 
 const Home = () => {
     return (
-        <div>
+        <div className="homePage">
             <Header/>
-            <img className="homeImg" src={homeImg}/>
-            <div className="brandSlider">Brand Logo Slider here</div>          
+            <img className="homeImg" src="https://images.unsplash.com/photo-1601924994987-69e26d50dc26?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+            <BrandCarousel/>    
+            <HomeCard/>    
         </div>
     );
 }
